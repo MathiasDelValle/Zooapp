ed24e866c8ea52b5eb9dea469898ade5ddc10134
Mathias del Valle <mdelvalletrpc@gmail.com>
2021-11-18 01:05:12 -0300

    se inicia estructura del proyecto MVC

---
+++ Modelos/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    class Animal : Model<Animal>
+    {
+
+        public bool save()
+        {
+            return true;
+        }
+
+        public Animal find(int id)
+        {
+            return this;
+        }
+    }
+}
